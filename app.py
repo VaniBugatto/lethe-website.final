7989b5fae29f14ffebe49bd9b229c5b3319f67d7
Vanina Bugatto <vaninabugatto@gmail.com>
2025-04-15 16:29:15 +0000

    esta es la que va

--- app.py
+++ app.py
@@ -20,8 +20,6 @@ st.set_page_config(
 # CSS estilo CyberPunk
 with open('style.css') as f:
     css = f.read()
-with open('config.toml') as f:
-    config = f.read()
 
 st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
 st.markdown("""
